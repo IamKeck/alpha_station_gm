acff8d23e01e41a8debc3956806daecea4b0e03c
Kent Kawanaka <meal.is.tharihyu@gmail.com>
2018-12-27 12:37:30 +0900

    テーブルの幅などを調整するように

--- src/Main.purs
+++ src/Main.purs
@@ -17,7 +17,7 @@ import Web.DOM.Document (toNode, toParentNode) as D
 import Web.DOM.Element (Element, fromNode, setAttribute, toNode, toParentNode)
 import Web.DOM.MutationObserver (MutationObserver, MutationObserverInitFields, mutationObserver, observe)
 import Web.DOM.MutationRecord (MutationRecord)
-import Web.DOM.Node (Node, appendChild, textContent)
+import Web.DOM.Node (Node, appendChild, firstChild, setTextContent, textContent)
 import Web.DOM.NodeList (toArray)
 import Web.DOM.ParentNode (QuerySelector(..), querySelector, querySelectorAll)
 import Web.DOM.Text (toNode) as T
@@ -39,8 +39,17 @@ runMayDom  = runMaybeT
 runMayDomUnit :: MaybeT Effect Unit -> Effect Unit
 runMayDomUnit  = runMaybeT >>> (pure unit <* _)
 
-searchInput :: QuerySelector
-searchInput = QuerySelector "input[name='q']"
+changeTableCss :: Document -> Effect Unit
+changeTableCss doc = runMayDomUnit do
+  let documentP = D.toParentNode doc
+  table <- MaybeT $ querySelector (QuerySelector ".table_style02") documentP
+  traceM table
+  liftEffect $ setAttribute "style" "table-layout: fixed;" table
+  titleTh <- MaybeT $ querySelector (QuerySelector "th.title") documentP
+  traceM titleTh
+  liftEffect $ setAttribute "style" "width: 32%;" titleTh
+  pure unit
+  
 
 appendTd :: Document -> Node -> Effect Unit
 appendTd d tr = runMayDomUnit do
@@ -49,8 +58,17 @@ appendTd d tr = runMayDomUnit do
   titleElem <- MaybeT $ querySelector (QuerySelector "td:nth-child(2)") trElmParent
   artistElem <- MaybeT $ querySelector (QuerySelector "td:nth-child(3)") trElmParent
   artist <- liftEffect <<< map trim <<< textContent <<< toNode $ artistElem
+  artistTextNode <- MaybeT $ firstChild (toNode artistElem)
+  liftEffect $ setTextContent "" artistTextNode
+  artistP <- liftEffect $ createElement "p" d
+  liftEffect $ setAttribute "style" "width: calc(100% - 150px)" artistP
+  artistPNode <- pure $ toNode artistP
+  liftEffect $ setTextContent artist artistPNode 
+  _ <- liftEffect $ appendChild artistPNode (toNode artistElem)
   title <- liftEffect $ textContent $ toNode titleElem
-  tdNode <- liftEffect $ toNode <$> createElement "td" d
+  tdElm <- liftEffect $ createElement "td" d
+  liftEffect $ setAttribute "style" "width: 10%;" tdElm 
+  tdNode <- pure $ toNode tdElm
   let tweet = createTweet title artist
   _ <- liftEffect $ createTweetButton tweet d >>= (_ `appendChild` tdNode) 
   _ <- liftEffect $ appendChild tdNode tr
@@ -61,9 +79,11 @@ createTweet title artist = "♪Now Listening \n" <> title <> " / " <> artist <> 
 
 appendTdHead :: Document -> Node ->Effect Unit
 appendTdHead d tr = do
-  tdNode <- toNode <$> createElement "th" d
-  _ <- createTextNode "Tweet!" d >>= T.toNode >>> (_ `appendChild` tdNode)
-  appendChild tdNode tr *> pure unit
+  thElm <- createElement "th" d
+  setAttribute "style" "width: 10%;" thElm 
+  let thNode = toNode thElm
+  _ <- createTextNode "Tweet!" d >>= T.toNode >>> (_ `appendChild` thNode)
+  appendChild thNode tr *> pure unit
 
 type MutationObserverRet = {record :: MutationRecord, observer :: MutationObserver}
 
@@ -97,6 +117,8 @@ createTweetButton t d = do
   let elmNode = toNode elm
   appendChild textNode elmNode *> pure elmNode
 
+
+
 main :: Effect Unit
 main = do
   mb <- window >>= document >>= HD.body
@@ -117,30 +139,30 @@ main = do
           flag <- new true
           loadScript <- new false
           runMutationObserver init topNode \obsRet -> do
-              liftEffect do
-                traceM (obsRet.record)
-                flagVal <- read flag
-                case flagVal of
-                  true -> do
-                    modify_ (const false) flag
-                    headTr <- querySelector (QuerySelector "thead tr") topParentNode
-                    case headTr of
-                      Nothing -> write true flag *> pure unit
-                      Just tr -> do 
-                        appendTdHead doc (toNode tr)
-                        trs <- 
-                          querySelectorAll (QuerySelector "tbody tr") topParentNode 
-                            >>= toArray
-                        _ <- traverse (appendTd doc) trs
-                        write true flag
-                        loads <- read loadScript
-                        case loads of
-                          true ->
-                            loadTwitter
-                          false -> do
-                            _ <- addTweetWidget doc body
-                            write true loadScript
-                        pure unit
-                  false ->
-                    pure unit
+              traceM (obsRet.record)
+              flagVal <- read flag
+              case flagVal of
+                true -> do
+                  changeTableCss doc
+                  modify_ (const false) flag
+                  headTr <- querySelector (QuerySelector "thead tr") topParentNode
+                  case headTr of
+                    Nothing -> write true flag *> pure unit
+                    Just tr -> do 
+                      appendTdHead doc (toNode tr)
+                      trs <- 
+                        querySelectorAll (QuerySelector "tbody tr") topParentNode 
+                          >>= toArray
+                      _ <- traverse (appendTd doc) trs
+                      write true flag
+                      loads <- read loadScript
+                      case loads of
+                        true ->
+                          loadTwitter
+                        false -> do
+                          _ <- addTweetWidget doc body
+                          write true loadScript
+                      pure unit
+                false ->
+                  pure unit
           pure unit
